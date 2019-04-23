2f4f91893098a832d5b2d075658bcff318b6ce03
Milexys <milexysbetancourt1@gmail.com>
2019-04-23 20:06:59 -0300

    Componente Card creado

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/App.scss';
 
+import Card from './Card';
+
 function App() {
   return (
     <div className="App">
-      
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles/Card.scss';
+
+const Card = () => {
+  
+  return (
+    <div className="Card">
+      <div className="Mural-size">
+        <div>
+          <p>1x</p>
+        </div>
+      </div>
+      <p>Default</p>
+      <p>9000 x 6000 px</p>
+    </div>
+  )
+}
+
+export default Card;
